4bd74976e2f32fce7780f1d739557ae6db22924b
Pierrick Rouxel <pierrick.rouxel@inconcept.fr>
2020-01-23 09:40:58 +0100

    Fix test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoApplicationTests {
 
 	@Value("${main-value:#{null}}")
